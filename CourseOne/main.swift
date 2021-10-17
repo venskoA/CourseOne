fca5b88827d76a24e3a7bfd9cf325685bea77a01
venskoA <venskoandrey@gmail.com>
2021-10-17 14:28:51 +0300

    Урок до функций

--- CourseOne/main.swift
+++ CourseOne/main.swift
@@ -7,8 +7,97 @@
 
 import Foundation
 
-var x: Int = 5
+// case
 
-x += 1
+//let a = 10
+//let b = 66
+//let c = 16
 
-print(x)
+//switch a {
+//case 8...13:
+//    print ("8...13")
+//case 10:
+//    print("10")
+//default:
+//    print("defoult")
+//}
+
+//switch (a, b, c){
+//case (1, 2, 3):
+//    print("case1")
+//case (10, 66, 16):
+//    print("case2")
+//default:
+//    print("default")
+//}
+
+//switch (a, b, c) {
+//case (10, 66, let x):
+//    print("case1", x)
+//case (66, let x, 10):
+//    print("cose2", x)
+//default:
+//    print("xz")
+//}
+
+//switch a {
+//case 8:
+//    print("case1")
+//case 10 where b == c:
+//    print("case2")
+//case 10 where b != c:
+//    print("case3", b)
+//case 10:
+//    print("cose4")
+//default:
+//    print("def")
+//}
+
+//    for
+
+//let array = [6, 13, 27, 18]
+////
+////for element in array {
+////    let x = element * 2
+////    print(element)
+////}
+////
+//
+//
+////for _ in 1...5 {
+////    print("повтор")
+////}
+//
+//for i in stride(from: 1, through: 10, by: 3) {  //  включая 10
+//    print(i)
+//}
+//
+//for i in stride(from: 1, to: 10, by: 3) { //  не включая 10
+//    print(i)
+//}
+//print("=================")
+//for i in array where i % 2 == 0 {
+//    print(i)
+//}
+
+
+//    while
+
+//let dataFuture = Data().addingTimeInterval(10)
+//print(dataFuture)
+//
+//while dataFuture > Data() {
+//    print(Data())
+//    sleep(1)
+//}
+
+// continue, break
+
+// Логические операторы
+// && || !
+
+
+
+
+
+
